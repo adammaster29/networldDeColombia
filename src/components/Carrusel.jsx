1364cc40fe9738df155f55fc613471f43e7c080b
adammaster29 <104658788+adammaster29@users.noreply.github.com>
2023-03-12 09:37:47 -0500

    actualizacion progress

--- src/components/Carrusel.jsx
+++ src/components/Carrusel.jsx
@@ -1,11 +1,14 @@
 import React from 'react';
 import Carousel from 'react-bootstrap/Carousel';
 const Carrusel = () => {
+
+  
+
   return (
 
     
     
-    <Carousel className='carousel' fade>
+    <Carousel className='carousel' fade interval='1400'>
       
       <Carousel.Item  >
         <img
@@ -15,8 +18,8 @@ const Carrusel = () => {
         />
         
         <Carousel.Caption className='center-text'>
-          <h3 className='animacion' style={{ fontSize: "45px", fontWeight: 900 }}> NETWORLD DE COLOMBIA S.A.S</h3>
-          <p className='animacion'  style={{ fontSize: "30px", fontWeight: 600 }}>En La Vanguardia De La Tecnología.</p>
+          <h3 className='animacion ' style={{ fontSize: "45px", fontWeight: 900 }}> NETWORLD DE COLOMBIA S.A.S</h3>
+          <p className='animacion progress-bar'  style={{ fontSize: "30px", fontWeight: 600 }}>En La Vanguardia De La Tecnología.</p>
           <div className='btn animacion' ><button  className='button-carrusel' >Ver Servicios</button> <button className='button-carrusel'>Leer Mas</button></div>
         </Carousel.Caption>
    
@@ -31,7 +34,7 @@ const Carrusel = () => {
        
         <Carousel.Caption className='center-text'>
           <h3 className='animacion' style={{ fontSize: "45px", fontWeight: 900 }}> NETWORLD DE COLOMBIA S.A.S</h3>
-          <p className='animacion'  style={{ fontSize: "30px", fontWeight: 600 }}>En La Vanguardia De La Tecnología.</p>
+          <p className='animacion progress-bar'  style={{ fontSize: "30px", fontWeight: 600 }}>En La Vanguardia De La Tecnología.</p>
           <div className='btn animacion'><button  className='button-carrusel' >Ver Servicios</button> <button className='button-carrusel'>Leer Mas</button></div>
         </Carousel.Caption>
         
@@ -46,7 +49,7 @@ const Carrusel = () => {
 
         <Carousel.Caption className='center-text'>
           <h3 className='animacion' style={{ fontSize: "45px", fontWeight: 900 }}> Cumplimos con los requerimientos de nuestros clientes.</h3>
-          <p className='animacion'  style={{ fontSize: "30px", fontWeight: 600 }}>Somos tu mejor opcion.</p>
+          <p className='animacion progress-bar'  style={{ fontSize: "30px", fontWeight: 600 }}>Somos tu mejor opcion.</p>
           <div className='btn animacion'><button  className='button-carrusel' >Ver Servicios</button> <button className='button-carrusel'>Leer Mas</button></div>
         </Carousel.Caption>
         
--- src/components/ProgresoCircular.jsx
+++ src/components/ProgresoCircular.jsx
@@ -1,12 +1,56 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
 const ProgresoCircular = () => {
+    const [progress, setProgress] = useState(0);
+    const [progress1, setProgress1] = useState(0);
+    const [progress2, setProgress2] = useState(0);
+
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      setProgress(prevProgress => {
+        if (prevProgress >= 79) {
+          clearInterval(intervalId);
+        }
+        return prevProgress + 1;
+      });
+    }, 50);
+
+    return () => clearInterval(intervalId);
+  }, []);
+//   PROGRESS1
+useEffect(() => {
+    const intervalId = setInterval(() => {
+      setProgress1(prevProgress => {
+        if (prevProgress >= 69) {
+          clearInterval(intervalId);
+        }
+        return prevProgress + 1;
+      });
+    }, 50);
+
+    return () => clearInterval(intervalId);
+  }, []);
+
+  //PROGRESS2
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      setProgress2(prevProgress => {
+        if (prevProgress >= 38) {
+          clearInterval(intervalId);
+        }
+        return prevProgress + 1;
+      });
+    }, 50);
+
+    return () => clearInterval(intervalId);
+  }, []);
+
 
     return (
         <div className='container-progresso'>
             <div  className='progressoflex animacion'   >
                 <div style={{ borderRadius: "50%", width: '170px', height: "150px" }} class="progress" role="progressbar" aria-label="Example with label" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100">
-                    <div class="progress-bar" style={{ width: "80%" }}>80%</div>
+                    <div class="progress-bar" style={{ width: `${progress}%` }}>{progress}%</div>
                    
                 </div>
                 <span>Mantenimiento de Redes</span>
@@ -14,7 +58,7 @@ const ProgresoCircular = () => {
 
             <div className='progressoflex animacion'>   
             <div style={{ borderRadius: "50%", width: '170px', height: "150px" }} class="progress" role="progressbar" aria-label="Example with label" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100">
-                <div class="progress-bar" style={{ width: "70%" }}>70%</div>
+            <div class="progress-bar" style={{ width: `${progress1}%` }}>{progress1}%</div>
                 
             </div>
             <span>Creación y Diseño WEB</span>
@@ -22,7 +66,7 @@ const ProgresoCircular = () => {
 
             <div className='progressoflex animacion'> 
             <div style={{ borderRadius: "50%", width: '170px', height: "150px" }} class="progress" role="progressbar" aria-label="Example with label" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100">
-                <div class="progress-bar" style={{ width: "39%" }}>39%</div>
+            <div class="progress-bar" style={{ width: `${progress2}%` }}>{progress2}%</div>
                
             </div>
             <span>Suministros de Equipos</span>
