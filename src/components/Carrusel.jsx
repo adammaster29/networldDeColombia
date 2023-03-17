e7d39582f21b5d6665e3417826bd73ff38542afc
adammaster29 <104658788+adammaster29@users.noreply.github.com>
2023-03-17 15:52:37 -0500

    responsive nav

--- src/components/Carrusel.jsx
+++ src/components/Carrusel.jsx
@@ -6,20 +6,20 @@ const Carrusel = () => {
 
   return (
 
+    // interval='1400'
     
-    
-    <Carousel className='carousel' fade interval='1400' >
+    <Carousel className='carousel' fade  >
       
       <Carousel.Item  >
         <img
-          className="d-block w-100"
+          className="d-block w-100 imgCarrusel"
           src="/img/img-redes.png"
           alt="First slide"
         />
         
-        <Carousel.Caption className='center-text'>
-          <h3 className='animacion ' style={{ fontSize: "45px", fontWeight: 900 }}> NETWORLD DE COLOMBIA S.A.S</h3>
-          <p className='animacion progress-bar'  style={{ fontSize: "30px", fontWeight: 600 }}>En La Vanguardia De La Tecnología.</p>
+        <Carousel.Caption className='mx-auto text-justify'>
+          <h3 className='animacion mx-auto text-justify display-6 fw-bold' > NETWORLD DE COLOMBIA S.A.S</h3>
+          <h3 className='animacion mx-auto text-break fw-bold'  >En La Vanguardia De La Tecnología.</h3>
           {/* <div className='btnB ' ></div> */}<br /><br />
           <button  className='button-carrusel animacion mg' >Ver Servicios</button> <button className='button-carrusel animacion mg'>Leer Mas</button>
         </Carousel.Caption>
@@ -28,14 +28,14 @@ const Carrusel = () => {
 
       <Carousel.Item>
         <img
-          className="d-block w-100"
+          className="d-block w-100 imgCarrusel"
           src="/img/img-network.jpg"
           alt="Second slide"
         />
        
-        <Carousel.Caption className='center-text'>
-          <h3 className='animacion' style={{ fontSize: "45px", fontWeight: 900 }}> NETWORLD DE COLOMBIA S.A.S</h3>
-          <p className='animacion progress-bar'  style={{ fontSize: "30px", fontWeight: 600 }}>En La Vanguardia De La Tecnología.</p>
+        <Carousel.Caption className='mx-auto text-justify'>
+          <h3 className='animacion mx-auto text-justify display-6 fw-bold' > NETWORLD DE COLOMBIA S.A.S</h3>
+          <h3 className='animacion mx-auto text-break fw-bold' >En La Vanguardia De La Tecnología.</h3>
           {/* <div className='btnB '></div> */} <br /><br />
           <button  className='button-carrusel animacion mg' >Ver Servicios</button> <button className='button-carrusel animacion mg'>Leer Mas</button>
         </Carousel.Caption>
@@ -44,14 +44,14 @@ const Carrusel = () => {
 
       <Carousel.Item className='Item'>
         <img
-          className="d-block w-100"
+          className="d-block w-100 imgCarrusel"
           src="/img/img-fibra.png"
           alt="Third slide"
         />
 
-        <Carousel.Caption className='center-text'>
-          <h3 className='animacion' style={{ fontSize: "45px", fontWeight: 900 }}> Cumplimos con los requerimientos de nuestros clientes.</h3>
-          <p className='animacion progress-bar'  style={{ fontSize: "30px", fontWeight: 600 }}>Somos tu mejor opcion.</p>
+        <Carousel.Caption className='mx-auto text-justify'>
+          <h3 className='animacion mx-auto text-justify display-6 fw-bold' > Cumplimos con los requerimientos de nuestros clientes.</h3>
+          <h3 className='animacion  mx-auto text-break  fw-bold'  >Somos tu mejor opcion.</h3>
           {/* <div className='btnB boton3 '></div> */}
           <button  className='button-carrusel animacion' >Ver Servicios</button> <button className='button-carrusel animacion'>Leer Mas</button>
         </Carousel.Caption>
--- src/components/Navegacion.jsx
+++ src/components/Navegacion.jsx
@@ -2,27 +2,29 @@ import React from 'react';
 
 const Navegacion = () => {
     return (
-<nav>
-  <ul className='navegador'>
-    <p className='textoNav'><span className='span-N'>N</span>etworld de Colombia S.A.S</p>
-    <li><a style={{color:"white"}} href="#">Inicio</a></li>
-    <li><a style={{color:"white"}} href="#acerca">Acerca de Nosotros</a></li>
-    <li><a style={{color:"white"}} href="#servicios">Servicios</a></li>
-    <li><a style={{color:"white"}} href="#equipo">Equipo</a></li>
-    <li><a style={{color:"white"}} href="#portafolio">Portafolio</a></li>
-    <li><a style={{color:"white"}} href="#cliente">Blog</a></li>
-    <li><a style={{color:"white"}} href="#contactos">Contactos</a></li>
-    <li><a style={{color:"white"}} >Empresa</a>
-    
-      <ul className='ulObject'>
-        <li><a href="#">Objectivos</a></li>
-        <li><a href="#">Mision 2</a></li>
-        <li><a href="#">Vision</a></li>
+      <nav>
+      <div class="logo"><p className='textoNav'><span className='span-N'>N</span>etworld de Colombia S.A.S</p></div>
+      <input type="checkbox" id="menu-toggle" />
+      <label for="menu-toggle" class="menu-icon">&#9776;</label>
+      <ul class="menu">
+        <li><a href="#">Inicio</a></li>
+        <li><a href="#">Contacto</a></li>
+        <li><a href="#">Inicio</a></li>
+        <li><a href="#">Contacto</a></li>
+        <li><a href="#">Inicio</a></li>
+        <li><a href="#">Contacto</a></li>
+        <li><a href="#">Contacto</a></li>
+        <li>
+          <a href="#">Productos</a>
+          <ul class="submenu">
+            <li><a href="#">Producto 1</a></li>
+            <li><a href="#">Producto 2</a></li>
+            <li><a href="#">Producto 3</a></li>
+          </ul>
+        </li>
+        
       </ul>
-    </li>
-    
-  </ul>
-</nav>
+    </nav>
 
     );
 };
